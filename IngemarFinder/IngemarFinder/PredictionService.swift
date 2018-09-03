0128c0f37d89c1b36900d5d4a7a7154d04af3ae2
Magnus Kraepelien <magnuskraepelien@Magnuss-MBP-2.lan>
2018-09-03 20:38:59 +0200

    Implement bundle setting for server side

--- IngemarFinder/IngemarFinder/PredictionService.swift
+++ IngemarFinder/IngemarFinder/PredictionService.swift
@@ -3,31 +3,47 @@ import AVKit
 import Vision
 import AVFoundation
 
+struct ResultViewModel {
+    let tagName: String?
+    let confidence: Float?
+}
+
 struct PredictionService {
 
-    func getPredictionFrom(_ image: UIImage, completion: @escaping ([IngemarPrediction]) -> ()){
+    func getPredictionFrom(_ image: UIImage, completion: @escaping (ResultViewModel) -> ()){
+        let serverMode = UserDefaults.standard.bool(forKey: "server_mode")
+        if serverMode {
+            getPredictionFromServerMachine(image) { (ingemarPredections) in
+            let viewModel = ResultViewModel(tagName: ingemarPredections.predictions.first?.tagName,
+                                            confidence: ingemarPredections.predictions.first?.probability)
+                completion(viewModel)
+            }
+        } else {
+            getResultFromLocalMachine(image) { (observation) in
+                let viewModel = ResultViewModel(tagName: observation.identifier, confidence: observation.confidence)
+                completion(viewModel)
+            }
+        }
+    }
+
+    //Use server side Model
+    func getPredictionFromServerMachine(_ image: UIImage, completion: @escaping (IngemarPredictions) -> ()){
         guard let compressData = UIImageJPEGRepresentation(image, 0.5) else { return }
         Worker().makeHTTPPost(data: compressData){ ingemarPredections in
             DispatchQueue.main.async(execute: {
-                completion(ingemarPredections.predictions)
-//                self?.spinner.stopAnimating()
-//                self?.showResult(name: ingemarPredections.predictions.first?.tagName, confidence: ingemarPredections.predictions.first?.probability)
+                completion(ingemarPredections)
             })
-            //print(ingemarPredections.predictions.first)
         }
-        
     }
-    func getResultFromLocalMachine(image: UIImage, completion: @escaping (IngemarPrediction) -> ()) {
-        guard let model = try? VNCoreMLModel(for: ingemar().model) else { return }
-        guard let pixelBuffer = buffer(from: image) else { return }
+    //Use local mlmodel
+    func getResultFromLocalMachine(_ image: UIImage, completion: @escaping (VNClassificationObservation) -> ()) {
+        guard let model = try? VNCoreMLModel(for: ingemar().model), let pixelBuffer = buffer(from: image) else { return }
 
         let request = VNCoreMLRequest(model: model) {(finishedRequest, error) in
             guard error == nil else { return }
-            guard let results = finishedRequest.results else { return }
-            guard let observation = results.first as? VNClassificationObservation else { return }
+            guard let results = finishedRequest.results, let observation = results.first as? VNClassificationObservation else { return }
             DispatchQueue.main.async(execute: {
-                let ingemarPrediction = IngemarPrediction(tagName: observation.identifier, probability: observation.confidence, tagId:"")
-                completion(ingemarPrediction)
+                completion(observation)
             })
         }
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
@@ -37,9 +53,8 @@ struct PredictionService {
         let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
         var pixelBuffer : CVPixelBuffer?
         let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard (status == kCVReturnSuccess) else {
-            return nil
-        }
+        
+        guard (status == kCVReturnSuccess) else { return nil }
 
         CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
         let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
@@ -49,7 +64,6 @@ struct PredictionService {
 
         context?.translateBy(x: 0, y: image.size.height)
         context?.scaleBy(x: 1.0, y: -1.0)
-
         UIGraphicsPushContext(context!)
         image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
         UIGraphicsPopContext()
